97f870f705e2a7ebea23ea0b23011ba043da4388
Fred Korts <fred.korts@wunder.io>
2018-10-25 21:23:38 +0300

    First commit.

---
+++ app/src/index.js
@@ -0,0 +1,5 @@
+let myName = "Fred";
+
+const myNameShout = (nameVar) => { console.log(`something ${nameVar}`) }
+
+myNameShout(myName);
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// require gulp module
+let gulp = require("gulp");
+let OUT_DIR = './build';
+let webpack = require('webpack-stream');
+
+// create a task to copy static html to the build folder
+gulp.task('copy:html', () => gulp.src('./app/template/index.html').pipe(gulp.dest(OUT_DIR)));
+
+// copy css to build folder
+gulp.task('copy:css', () => gulp.src('./app/template/style.css').pipe(gulp.dest(OUT_DIR)));
+
+// if any assets are present, copy them to the build folder as well
+gulp.task('copy:assets', () => gulp.src('./app/assets/*').pipe(gulp.dest(OUT_DIR + '/assets/')));
+
+gulp.task('webpack', () => {
+  return gulp.src('build/')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('build/'));
+});
+
+// Default gulp task, runs all previously defined tasks.
+gulp.task('default', ['copy:html', 'copy:css', 'copy:assets', 'webpack']);
